dd92fa55572763c643928fa90f6c19df97b99cd4
Ying-an(Ethan) Yuan <yyajiayou@gmail.com>
2025-06-28 20:22:10 +0800

    avp-slam git repo init

---
+++ state_estimation/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ubuntu/avp-slam/state_estimation/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/ubuntu/avp-slam/state_estimation/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/ubuntu/avp-slam/state_estimation/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/ubuntu/avp-slam/state_estimation/src:$ROS_PACKAGE_PATH"
---
+++ state_estimation/build/vehicle_state_estimation/CMakeFiles/vehicle_state_estimation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/ubuntu/avp-slam/state_estimation/devel/lib/vehicle_state_estimation/vehicle_state_estimation"
+  "/home/ubuntu/avp-slam/state_estimation/devel/lib/vehicle_state_estimation/vehicle_state_estimation.pdb"
+  "CMakeFiles/vehicle_state_estimation.dir/src/vehicle_state_estimation_node.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vehicle_state_estimation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
