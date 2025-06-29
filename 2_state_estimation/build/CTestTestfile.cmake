cfc538f90ab15772c324fcdb202707e5d6a75929
Ying-an(Ethan) Yuan <yyajiayou@gmail.com>
2025-06-29 11:19:33 +0800

    init .gitignorea and ignore `**/build/` and `**/devel/`

--- 2_state_estimation/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/avp-slam/state_estimation/src
-# Build directory: /home/ubuntu/avp-slam/state_estimation/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("vehicle_state_estimation")
